836c232e830e4bb5d90046fbbb33e0d8f2735b38
Abel Dustin Hyman Susilo <108639591+adhxabre@users.noreply.github.com>
2022-12-13 10:53:41 +0700

    Update uploadFile.go

--- server/pkg/middleware/uploadFile.go
+++ server/pkg/middleware/uploadFile.go
@@ -11,15 +11,15 @@ import (
 )
 
 func UploadFile(next http.HandlerFunc) http.HandlerFunc {
-  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    // Upload file
-    // FormFile returns the first file for the given key `myFile`
-    // it also returns the FileHeader so we can get the Filename,
-    // the Header and the size of the file
-    file, _, err := r.FormFile("image")
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Upload file
+		// FormFile returns the first file for the given key `myFile`
+		// it also returns the FileHeader so we can get the Filename,
+		// the Header and the size of the file
+		file, _, err := r.FormFile("image")
 
 		if err != nil && r.Method == "PATCH" {
-			ctx := context.WithValue(r.Context(), "dataFile", "false" )
+			ctx := context.WithValue(r.Context(), "dataFile", "false")
 			next.ServeHTTP(w, r.WithContext(ctx))
 			return
 		}
@@ -32,8 +32,8 @@ func UploadFile(next http.HandlerFunc) http.HandlerFunc {
 		}
 		defer file.Close()
 
-    // setup file type filtering
-    buff := make([]byte, 512)
+		// setup file type filtering
+		buff := make([]byte, 512)
 		_, err = file.Read(buff)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
@@ -58,38 +58,44 @@ func UploadFile(next http.HandlerFunc) http.HandlerFunc {
 			return
 		}
 
+		// setup max-upload
+		const MAX_UPLOAD_SIZE = 10 << 20
+		r.ParseMultipartForm(MAX_UPLOAD_SIZE)
+		if r.ContentLength > MAX_UPLOAD_SIZE {
+			w.WriteHeader(http.StatusBadRequest)
+			response := Result{Code: http.StatusBadRequest, Message: "Max size in 1mb"}
+			json.NewEncoder(w).Encode(response)
+			return
+		}
 
-    // fmt.Printf("Uploaded File: %+v\n", handler.Filename)
-    // fmt.Printf("File Size: %+v\n", handler.Size)
-    // fmt.Printf("MIME Header: %+v\n", handler.Header)
-    const MAX_UPLOAD_SIZE = 10 << 20 // 10MB
-    // Parse our multipart form, 10 << 20 specifies a maximum
-    // upload of 10 MB files.
-    r.ParseMultipartForm(MAX_UPLOAD_SIZE)
-    // if r.ContentLength > MAX_UPLOAD_SIZE {
-    //   w.WriteHeader(http.StatusBadRequest)
-    //   response := Result{Code: http.StatusBadRequest, Message: "Max size in 1mb"}
-    //   json.NewEncoder(w).Encode(response)
-    //   return
-    // }
+		// Create a temporary file within our temp-images directory that follows
+		// a particular naming pattern
+		tempFile, err := ioutil.TempFile("uploads", "image-*.png")
+		if err != nil {
+			fmt.Println(err)
+			fmt.Println("path upload error")
+			json.NewEncoder(w).Encode(err)
+			return
+		}
+		defer tempFile.Close()
 
-    // Create a temporary file within our temp-images directory that follows
-    // a particular naming pattern
-    tempFile, err := ioutil.TempFile("uploads", "image-*.png")
-    if err != nil {
-      fmt.Println(err)
-      fmt.Println("path upload error")
-      json.NewEncoder(w).Encode(err)
-      return
-    }
-    defer tempFile.Close()
+		// read all of the contents of our uploaded file into a
+		// byte array
+		fileBytes, err := ioutil.ReadAll(file)
+		if err != nil {
+			fmt.Println(err)
+		}
 
-    // read all of the contents of our uploaded file into a
-    // byte array
-    fileBytes, err := ioutil.ReadAll(file)
-    if err != nil {
-      fmt.Println(err)
-    }
+		// write this byte array to our temporary file
+		tempFile.Write(fileBytes)
+
+		data := tempFile.Name()
+
+		// add filename to ctx
+		ctx := context.WithValue(r.Context(), "dataFile", data)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
 
     // write this byte array to our temporary file
     tempFile.Write(fileBytes)
